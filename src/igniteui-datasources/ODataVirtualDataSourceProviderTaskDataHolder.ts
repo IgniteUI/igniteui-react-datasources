55e701aa8afdf490046109e2fb9716f6ac716ca5
gmurray81 <gmurray@gmail.com>
2019-11-11 16:28:34 -0500

    move to submodule

--- src/igniteui-datasources/ODataVirtualDataSourceProviderTaskDataHolder.ts
+++
@@ -1,8 +0,0 @@
-import { AsyncVirtualDataSourceProviderTaskDataHolder } from "igniteui-core/AsyncVirtualDataSourceProviderTaskDataHolder";
-import { Base, Type, markType } from "igniteui-core/type";
-
-export class ODataVirtualDataSourceProviderTaskDataHolder extends AsyncVirtualDataSourceProviderTaskDataHolder {
-	static $t: Type = markType(ODataVirtualDataSourceProviderTaskDataHolder, 'ODataVirtualDataSourceProviderTaskDataHolder', (<any>AsyncVirtualDataSourceProviderTaskDataHolder).$type);
-}
-
-
